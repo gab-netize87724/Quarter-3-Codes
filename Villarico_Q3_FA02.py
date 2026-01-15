c343855bbf3b263ae4818c1284d4e441f8b1bb3f
Gabrielle Villarico <gabriellejulia.villarico@smc.pshs.edu.ph>
2026-01-15 22:05:14 +0800

    Create Villarico_Q3_FA02.py
    Uploaded Quarter 3 - FA 02 (Based on SG4: Activity 1: Planning a Mini-Dataset Project) code.

---
+++ Villarico_Q3_FA02.py
@@ -0,0 +1,16 @@
+scores = [
+    [85, 90, 88],  
+    [78, 82, 80],  
+    [92, 95, 94],  
+    [70, 75, 72],  
+    [88, 86, 90]   
+]
+
+print("Student 3's Science score:", scores[2][1])
+
+print("Student 1's scores...", scores[0])
+
+print("Updating Student 4's Math score to 75.")
+scores[3][0] = 75
+
+print("Student 4's updated scores:", scores[3])
